ad679450ea8b29530c471e71da9520d2a87b2a95
Whu-wxy <641645560@qq.com>
2020-03-14 17:30:07 +0800

    fix cpp

--- pse/pse.cpp
+++ pse/pse.cpp
@@ -15,32 +15,32 @@ namespace py = pybind11;
 namespace pse{
     //S5->S0, small->big
     std::vector<std::vector<int32_t>> pse(
-    py::array_t<int32_t, py::array::c_style> region,
-    py::array_t<uint8_t, py::array::c_style> center)
+    py::array_t<int32_t, py::array::c_style> center,
+    py::array_t<uint8_t, py::array::c_style> region)
     {
-        auto pbuf_region = region.request();
         auto pbuf_center = center.request();
-        if (pbuf_region.ndim != 2 || pbuf_region.shape[0]==0 || pbuf_region.shape[1]==0)
+        auto pbuf_region = region.request();
+        if (pbuf_center.ndim != 2 || pbuf_center.shape[0]==0 || pbuf_center.shape[1]==0)
             throw std::runtime_error("label map must have a shape of (h>0, w>0)");
-        int h = pbuf_region.shape[0];
-        int w = pbuf_region.shape[1];
-        if (pbuf_center.ndim != 3 || pbuf_center.shape[1]!=h || pbuf_center.shape[2]!=w)
-            throw std::runtime_error("center must have a shape of (c>0, h>0, w>0)");
+        int h = pbuf_center.shape[0];
+        int w = pbuf_center.shape[1];
+        if (pbuf_center.ndim != 2 || pbuf_region.shape[0]!=h || pbuf_region.shape[1]!=w)
+            throw std::runtime_error("region must have a shape of (h>0, w>0)");
 
         std::vector<std::vector<int32_t>> res;
         for (size_t i = 0; i<h; i++)
             res.push_back(std::vector<int32_t>(w, 0));
-        auto ptr_region = static_cast<int32_t *>(pbuf_region.ptr);
-        auto ptr_center = static_cast<uint8_t *>(pbuf_center.ptr);
+        auto ptr_center = static_cast<int32_t *>(pbuf_center.ptr);
+        auto ptr_region = static_cast<uint8_t *>(pbuf_region.ptr);
 
         std::queue<std::tuple<int, int, int32_t>> q, next_q;
 
         for (size_t i = 0; i<h; i++)
         {
-            auto p_region = ptr_region + i*w;
+            auto p_center = ptr_center + i*w;
             for(size_t j = 0; j<w; j++)
             {
-                int32_t label = p_region[j];
+                int32_t label = p_center[j];
                 if (label>0)
                 {
                     q.push(std::make_tuple(i, j, label));
@@ -68,7 +68,7 @@ namespace pse{
                 int index_x = x + dx[idx];
                 if (index_y<0 || index_y>=h || index_x<0 || index_x>=w)
                     continue;
-                if (!ptr_center[index_y*w+index_x] || res[index_y][index_x]>0)
+                if (!ptr_region[index_y*w+index_x] || res[index_y][index_x]>0)
                     continue;
                 q.push(std::make_tuple(index_y, index_x, l));
                 res[index_y][index_x]=l;
@@ -80,5 +80,5 @@ namespace pse{
 }
 
 PYBIND11_MODULE(pse, m){
-    m.def("pse_cpp", &pse::pse, " re-implementation pse algorithm(cpp)", py::arg("region"), py::arg("center"));
+    m.def("pse_cpp", &pse::pse, " re-implementation pse algorithm(cpp)", py::arg("center"), py::arg("region"));
 }

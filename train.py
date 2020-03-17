19c4c8c7bd1fd5ab76d66b4f78d8ff85c5d6ef04
Whu-wxy <641645560@qq.com>
2020-03-17 12:42:03 +0800

    Update train.py

--- train.py
+++ train.py
@@ -410,7 +410,7 @@ if __name__ == '__main__':
     import utils
 
     model = GFF_FPN(backbone=config.backbone, pretrained=config.pretrained, result_num=config.n)
-    model = FPN_ResNet(backbone=config.backbone, pretrained=config.pretrained, result_num=config.n)
+    #model = FPN_ResNet(backbone=config.backbone, pretrained=config.pretrained, result_num=config.n)
 
     # model = SA_FPN(backbone=config.backbone, pretrained=config.pretrained, result_num=config.n)
 

d5856ea9ef7fb08e3f8d544777e393220feee7b1
Whu-wxy <641645560@qq.com>
2020-03-18 10:19:14 +0800

    Update eval.py

--- eval.py
+++ eval.py
@@ -53,18 +53,18 @@ if __name__ == '__main__':
     backbone = 'resnet50'  #res2net50_26w_6s   res2net_dla60
     long_size = 1900     #2240
     scale = 2
-    model_path = '../save/dist_gff/Best_624_r0.636976_p0.580518_f10.607438.pth'
-    #../data/PSENet_Resnet_on_ic15/resnet152.pth  #Best_638_r0.584497_p0.829235_f10.685682.pth
-    #res2net50_26w_6s_2.1/train_PSENet_524_loss0.347171.pth
-    #../save/CV/res2net_dla2.0/Best_638_r0.584497_p0.829235_f10.685682.pth
+    model_path = '../Best_558_r0.662494_p0.583793_f10.620659.pth'
+
+    #../ save / dist_gff / Best_624_r0.636976_p0.580518_f10.607438.pth
+
     data_path = '../IC15/test/img'
     gt_path = '../IC15/test/gt'   # gt_2pts, gt
-    save_path = '../save/test_result2'
+    save_path = '../save/test_result'
     gpu_id = 0
     print('backbone:{},scale:{},model_path:{}'.format(backbone,scale,model_path))
 
-    net = GFF_FPN(backbone=backbone, pretrained=False, result_num=config.n)
-    #net = FPN_ResNet(backbone=backbone, pretrained=False, result_num=config.n)
+    #net = GFF_FPN(backbone=backbone, pretrained=False, result_num=config.n)
+    net = FPN_ResNet(backbone=backbone, pretrained=False, result_num=config.n)
 
     save_path = main(net, model_path, backbone, long_size, scale, data_path, save_path, gpu_id=gpu_id)
 

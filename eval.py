4428eca31e47533680576734e6bc4471b9d3b45f
Whu-wxy <641645560@qq.com>
2020-03-20 20:13:09 +0800

    bd

--- eval.py
+++ eval.py
@@ -52,15 +52,15 @@ if __name__ == '__main__':
     os.environ['CUDA_VISIBLE_DEVICES'] = str('0')
     backbone = 'resnet50'  #res2net50_26w_6s   res2net_dla60
     long_size = 1900     #2240
-    scale = 2
+    scale = 1
     eval_script = 'iou'
-    model_path = '../Best_558_r0.662494_p0.583793_f10.620659.pth'
+    model_path = '../save/dist_bd/Best_562_r0.656235_p0.582230_f10.617021.pth'
 
     #../ save / dist_gff / Best_624_r0.636976_p0.580518_f10.607438.pth
 
     data_path = '../IC15/test/img'
     gt_path = '../IC15/test/gt'   # gt_2pts, gt
-    save_path = '../save/test_result2'
+    save_path = '../save/test_result'
     gpu_id = 0
     print('backbone:{},scale:{},model_path:{}'.format(backbone,scale,model_path))
 

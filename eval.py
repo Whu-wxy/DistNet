5551ed4c7dcafccd28f1254c738fc2325c52cc31
Whu-wxy <641645560@qq.com>
2020-03-25 22:40:51 +0800

    1

--- eval.py
+++ eval.py
@@ -52,7 +52,7 @@ if __name__ == '__main__':
     os.environ['CUDA_VISIBLE_DEVICES'] = str('0')
     backbone = 'resnet50'  #res2net50_26w_6s   res2net_dla60
     long_size = 1900     #2240
-    scale = 4
+    scale = 2
     eval_script = 'iou'
     model_path = '../Best_558_r0.662494_p0.583793_f10.620659.pth'
 

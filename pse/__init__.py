11498fa3dc99abe352f5ee8c8f10b5b6652b38f8
Whu-wxy <641645560@qq.com>
2020-03-25 20:55:15 +0800

    1

--- pse/__init__.py
+++ pse/__init__.py
@@ -30,7 +30,7 @@ def pse_warpper(region, center, min_area=5, probs=None):
             continue
 
         score_i = np.mean(probs[label == label_idx])   #测试是否可以过滤难负样本
-        if score_i < 0.85:
+        if score_i < 0.9:
             continue
 
         label_values.append(label_idx)

54614d07d84d9e32a71f69341a3da043a28cf5de
Whu-wxy <641645560@qq.com>
2020-03-28 21:09:58 +0800

    11

--- pse/__init__.py
+++ pse/__init__.py
@@ -30,7 +30,7 @@ def pse_warpper(region, center, min_area=5, probs=None):
             continue
 
         score_i = np.mean(probs[label == label_idx])   #测试是否可以过滤难负样本
-        if score_i < 0.82:
+        if score_i < 0.8:
             continue
 
         label_values.append(label_idx)
@@ -181,7 +181,7 @@ def decode(preds, scale, threshold=config.decode_threld):  # origin=0.7311
     #
 
     region = preds >= 0.3
-    center = preds >= 0.7  #config.max_threld
+    center = preds >= 0.65  #config.max_threld
 
     #
     #

12925f0e0e75debea876b5d8237938bea4184491
Whu-wxy <641645560@qq.com>
2020-03-28 20:13:52 +0800

    Update eval.py

--- eval.py
+++ eval.py
@@ -51,8 +51,8 @@ if __name__ == '__main__':
     from models.GFF_FPN import GFF_FPN
     os.environ['CUDA_VISIBLE_DEVICES'] = str('0')
     backbone = 'resnet50'  #res2net50_26w_6s   res2net_dla60
-    long_size = 2240     #2240
-    scale = 1
+    long_size = 1900     #2240
+    scale = 2
     eval_script = 'iou'
     model_path = '../save/dist_biregion/Best_460_r0.654309_p0.622253_f10.637878.pth'
 

c834214d565822f55ca4afe85e77b77608723a4f
Whu-wxy <641645560@qq.com>
2020-03-28 22:39:34 +0800

    1

--- models/craft.py
+++ models/craft.py
@@ -46,8 +46,8 @@ class CRAFT(nn.Module):
             nn.Conv2d(32, 32, kernel_size=3, padding=1), nn.ReLU(inplace=True),
             nn.Conv2d(32, 32, kernel_size=3, padding=1), nn.ReLU(inplace=True),
             nn.Conv2d(32, 16, kernel_size=3, padding=1), nn.ReLU(inplace=True),
-            nn.Conv2d(16, num_out, kernel_size=1),   #nn.ReLU(inplace=True),
-            #nn.Conv2d(16, num_out, kernel_size=1),
+            nn.Conv2d(16, 16, kernel_size=1), nn.ReLU(inplace=True),
+            nn.Conv2d(16, num_out, kernel_size=1),
         )
 
         init_weights(self.upconv1.modules())

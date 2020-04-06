6ee8174d435e179a65f56884610dae6a1a21c759
Whu-wxy <641645560@qq.com>
2020-04-06 13:07:33 +0800

    1

--- pse/__init__.py
+++ pse/__init__.py
@@ -207,7 +207,7 @@ def decode(preds, scale, threshold=config.decode_threld):  # origin=0.7311
         points = np.array(np.where(pred == label_value)).transpose((1, 0))[:, ::-1]
 
         score_i = np.mean(bi_region[pred == label_value])
-        if score_i < 0.977:
+        if score_i < 0.979:
             continue
 
 

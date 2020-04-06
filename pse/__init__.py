40051f9061200c4717756f89e19614fdd3073c6a
Whu-wxy <641645560@qq.com>
2020-04-06 15:42:25 +0800

    1

--- pse/__init__.py
+++ pse/__init__.py
@@ -176,7 +176,7 @@ def decode(preds, scale, threshold=config.decode_threld):  # origin=0.7311
     preds = preds + bi_region - 1
     #
 
-    region = preds >= 0.31
+    region = preds >= 0.29
     center = preds >= 0.58  #config.max_threld
 
     #

2bc0db17c2bcb8fddd248df584d2820629f65bfa
Whu-wxy <641645560@qq.com>
2021-11-03 15:36:59 +0800

    20211103

--- dataset/cp_batches.py
+++ dataset/cp_batches.py
@@ -231,8 +231,8 @@ if __name__ == '__main__':
     # cvt_15('F:\\zzxs\\Experiments\\dl-data\\ICDAR\\ICDAR2015\\sample_IC15\\train',
     #        'F:\\zzxs\\Experiments\\dl-data\\ICDAR\\ICDAR2015\\sample_IC15\\train_res')
 
-    cvt_curve('F:\\zzxs\\Experiments\\dl-data\\TotalText\\sample2',
-              'F:\\zzxs\\Experiments\\dl-data\\TotalText\\res2', 'total', 1, True)
+    cvt_curve('F:\\zzxs\\Experiments\\dl-data\\TotalText\\sample',
+              'F:\\zzxs\\Experiments\\dl-data\\TotalText\\res', 'total', 1, True)
 
     # cvt_curve('F:\zzxs\Experiments\dl-data\CTW\ctw1500\sample',
     #           'F:\zzxs\Experiments\dl-data\CTW\ctw1500\\res', 'ctw1500', True)
